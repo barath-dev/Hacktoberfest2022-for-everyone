d92e9fad30b1ff124f8547a743cf4937d17d823b
barathwajgit <78818645+barathwajgit@users.noreply.github.com>
2022-10-12 02:16:58 +0530

    Update reverse-a-string.py

--- Python/reverse-a-string.py
+++ Python/reverse-a-string.py
@@ -2,6 +2,6 @@ def reverse(str):
     str = str[::-1]   
     return str   
     
-s = "Jarvis"  
+s = input() 
 print ("The original string  is : ",s)   
 print ("The reversed string using extended slice operator  is : ",reverse(s))  
